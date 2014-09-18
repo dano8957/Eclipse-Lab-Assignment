c01fc22b59f1ba4ca7f8b112ce5a84787ee25c70
dano8957 <daniel.j.nolan@colorado.edu>
2014-09-17 18:27:14 -0600

    Hello World

--- Hello World Practice/src/helloworld/Helloworld.java
+++ Hello World Practice/src/helloworld/Helloworld.java
@@ -5,5 +5,5 @@ public class Helloworld {
 	public static void main(String[] args) {
         System.out.println("Hello World!"); //Display the string.
         System.out.println("How are you?"); 
-}
+    }
 }
