3f45056e54def973ad6388242fcd02a7e03845c2
MariusVentus <jatolbonecrusher@hotmail.com>
2014-09-17 18:18:48 -0600

    Changes

--- Hello World Practice/src/helloworld/Helloworld.java
+++ Hello World Practice/src/helloworld/Helloworld.java
@@ -4,5 +4,6 @@ public class Helloworld {
 
 	public static void main(String[] args) {
         System.out.println("Hello World!"); //Display the string.
+        System.out.println("How are you?"); 
 }
 }
